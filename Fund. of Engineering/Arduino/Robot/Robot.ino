6919078a527d77cbcaf58a17c3b1c2e749d614f9
Your Name <naturesninja78@gmail.com>
2022-04-18 14:18:27 -0400

    things and stuff

--- Fund. of Engineering/Arduino/Robot/Robot.ino
+++ Fund. of Engineering/Arduino/Robot/Robot.ino
@@ -2,9 +2,10 @@
 
 Servo left;
 Servo right;
-const int sensIn=11;
-const int sensOut=10;
-long duration, inches;
+#define echoPin 2
+#define trigPin 3
+long duration; // duration of sound wave travel
+int distance; // distance measurement
 int consec=0;
 
 void setup() {
@@ -13,18 +14,23 @@ void setup() {
   right.attach(8);
   left.write(90);
   right.write(90);
+  flag.attach(2);
+  flag.write(90);
+  flag.write(0);
   Serial.begin(9600);
-  
+  pinMode(trigPin,OUTPUT);
+  pinMode(echoPin,INPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  if detectHazard() {
-    if consec >= 2 {
+  if (detectHazard()) {
+    if (flag
+    if (consec >= 2) {
       backward();
       consec+=1;
     }
-    else if consec > 0 {
+    else if (consec > 0) {
       rightTurn();
       consec+=1;
     }
@@ -69,7 +75,41 @@ int rightTurn() {
 
 bool detectHazard() {
   // Returns true if there is a hazard
+  // Clears the trigPin condition
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  // Sets the trigPin HIGH (ACTIVE) for 10 microseconds
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  // Reads the echoPin, returns the sound wave travel time in microseconds
+  duration = pulseIn(echoPin, HIGH);
+  // Calculating the distance
+  distance = duration * 0.034 / 2; // Speed of sound wave divided by 2 (go and back)
+  // Displays the distance on the Serial Monitor
+  Serial.print("Distance: ");
+  Serial.print(distance);
+  Serial.println(" cm");
+  if (distance < 20) {
+    return true;
+  }
+  return false;
 }
+
+class flag() {
+  public:
+  void up();
+  void down();
+  int pos();
+  
+  private:
+  Servo flag;
+};
+
+void flag::up() {
+  flag
+}
+
 
 /* Game Plan
  *  
@@ -86,4 +126,8 @@ bool detectHazard() {
  *    Green if moving
  *    Blue if stopped
  *    Red if hazard detected
+ *    
+ *    4. Flag
+ *    Up if hazard detected
+ *    Down otherwise
  */
--- Fund. of Engineering/Arduino/Sensor/Sensor.ino
+++ Fund. of Engineering/Arduino/Sensor/Sensor.ino
@@ -1,23 +1,31 @@
-const int sensIn= 11;
-const int sensOut= 10;
-long duration, inches;
+#define echoPin 2 // attach pin D2 Arduino to pin Echo of HC-SR04
+#define trigPin 3 //attach pin D3 Arduino to pin Trig of HC-SR04
+
+// defines variables
+long duration; // variable for the duration of sound wave travel
+int distance; // variable for the distance measurement
 
 void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(9600);
+  pinMode(trigPin, OUTPUT); // Sets the trigPin as an OUTPUT
+  pinMode(echoPin, INPUT); // Sets the echoPin as an INPUT
+  Serial.begin(9600); // // Serial Communication is starting with 9600 of baudrate speed
+  Serial.println("Ultrasonic Sensor HC-SR04 Test"); // print some text in Serial Monitor
+  Serial.println("with Arduino UNO R3");
 }
-
 void loop() {
-  // put your main code here, to run repeatedly:
-  pinMode(sensIn, INPUT); // Set pin to INPUT
-  pinMode(sensOut, OUTPUT); // Set pin to OUTPUT
-  digitalWrite(sensOut, LOW); // Turn ping off
+  // Clears the trigPin condition
+  digitalWrite(trigPin, LOW);
   delayMicroseconds(2);
-  digitalWrite(sensOut, HIGH); // Sends out ultrasonic pulse
-  delayMicroseconds(5); // 5 microsecond burst
-  digitalWrite(sensOut, LOW); // End of burst, turn off ping
-  duration = pulseIn(sensIn, HIGH); // Measure elapsed time
-  inches = duration/74/2; // Convert time to distance in inches
-  Serial.println(inches); // Print line to Monitor
-  delay(200);
+  // Sets the trigPin HIGH (ACTIVE) for 10 microseconds
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  // Reads the echoPin, returns the sound wave travel time in microseconds
+  duration = pulseIn(echoPin, HIGH);
+  // Calculating the distance
+  distance = duration * 0.034 / 2; // Speed of sound wave divided by 2 (go and back)
+  // Displays the distance on the Serial Monitor
+  Serial.print("Distance: ");
+  Serial.print(distance);
+  Serial.println(" cm");
 }
