0ccddd78884db80cdd6c9b8138325bb6956e89b3
Gage Farmer <naturesninja78@gmail.com>
2022-09-22 12:18:22 -0400

    code-ing

---
+++ Autumn 2022/Programming C++/Program Notes/sum10.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	double total = 0, num;
+	
+	for (int i = 0; i < 10; i++) {
+	
+		cin >> num;
+		
+		total += num;
+		
+	}
+	
+	cout << total << endl;
+	
+	return 0;
+	
+}
