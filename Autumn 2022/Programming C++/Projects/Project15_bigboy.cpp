1076da165592a440217c0d04147284039edc1410
Gage Farmer <naturesninja78@gmail.com>
2022-11-17 12:15:54 -0500

    Update Project15_bigboy.cpp

--- Autumn 2022/Programming C++/Projects/Project15_bigboy.cpp
+++ Autumn 2022/Programming C++/Projects/Project15_bigboy.cpp
@@ -28,20 +28,28 @@ void printMinMax(int min1, int max1, int min2, int max2);
 
 int main() {
    // init vars
-	int year1Grades[MAX_GRADES];                          // grade array of each year
-	int year2Grades[MAX_GRADES];
-	int numGrades1(-1), numGrades2(-1);                   // number of grades in the lists
-	int avg(-1), min1(-1), max1(-1), min2(-1), max2(-1);  // average, minimum, and maximum grades
-	int numOutliers(-1), stdDev(-1);                      // number of outliers and standard deviation
+	int gradeList1[MAX_GRADES];             // grade array of each year
+	int gradeList2[MAX_GRADES];
+	int numGrades1(-1), numGrades2(-1);     // number of grades in the lists
+	int avg1(-1), min1(-1), max1(-1), 		// average, minimum, and maximum grades
+	int avg2(-1), min2(-1), max2(-1);  		
+	int numOutliers1(-1), stdDev1(-1);        // number of outliers and standard deviation
+	int numOutliers2(-1), stdDev2(-1);        
 	
 	// get grades
-	numGrades1 = getGrades(year1Grades, 1);
-	numGrades2 = getGrades(year2Grades, 2);
+	numGrades1 = getGrades(gradeList1, 1);
+	numGrades2 = getGrades(gradeList2, 2);
+	
+	// calculate stats
+	computeStats(1, gradeList1, numGrades1, avg1, min1, max1, stdDev1, numOutliers1);
+	computeStats(2, gradeList2, numGrades2, avg2, min2, max2, stdDev2, numOutliers2);
 	
 	// empty line
 	cout << endl;
 	
 	printHeader();
+	printStats(1, gradeList1, numGrades1, avg1, stdDev1);
+	printStats(2, gradeList2, numGrades2, avg2, stdDev2);
 	
 	
 	return 0;
@@ -70,10 +78,9 @@ void printHeader() {
 
 
 // print stats of a given year
-void printStats(const int year, const int[] gradeList, const int numGrades, int min, int max) {
-   int avg(-1);
-   computeStats
-   cout << setw(4) << year << setw(7) << avg
+void printStats(const int year, const int[] gradeList, const int numGrades, int avg, int stdDev) {
+   
+   cout << setw(4) << year << setw(7) << avg << 
    
 }
 
