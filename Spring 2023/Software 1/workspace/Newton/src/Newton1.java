f3dd3e6a8ca623cd90e03431af0a95ef23c1a1e5
Gage Farmer <naturesninja78@gmail.com>
2023-01-23 16:16:59 -0500

    proyecto

---
+++ Spring 2023/Software 1/workspace/Newton/src/Newton1.java
@@ -0,0 +1,126 @@
+import components.simplereader.SimpleReader;
+import components.simplereader.SimpleReader1L;
+import components.simplewriter.SimpleWriter;
+import components.simplewriter.SimpleWriter1L;
+import components.utilities.FormatChecker;
+
+/**
+ * Gets input from user and square roots it using newton iteration.
+ *
+ * @author Gage Farmer
+ *
+ */
+public final class Newton1 {
+
+    /**
+     * No argument constructor--private to prevent instantiation.
+     */
+    private Newton1() {
+    }
+
+    /**
+     * Main method.
+     *
+     * @param args
+     *            the command line arguments
+     */
+    public static void main(String[] args) {
+        SimpleReader in = new SimpleReader1L();
+        SimpleWriter out = new SimpleWriter1L();
+        boolean looping = true;
+
+        while (looping) {
+            printRoot(in, out);
+
+            looping = askLoop(in, out);
+        }
+
+        in.close();
+        out.close();
+    }
+
+    /**
+     * Asks user if they want to continue.
+     *
+     * @param in
+     *            input stream
+     * @param out
+     *            output stream
+     * @return if the loop will continue or not
+     */
+    private static boolean askLoop(SimpleReader in, SimpleWriter out) {
+        boolean res = false;
+
+        System.out.print("Enter Y to calculate another square root: ");
+        String ans = in.nextLine();
+
+        if (ans.toLowerCase().equals("y")) {
+            res = true;
+        }
+
+        return res;
+    }
+
+    /**
+     * Gets number input from user.
+     *
+     * @param in
+     *            input stream
+     * @param out
+     *            output stream
+     * @return valid number
+     */
+    private static double getNumber(SimpleReader in, SimpleWriter out) {
+        String input;
+        double answer = -1.0;
+        boolean invalid = true;
+
+        System.out.println("Enter a number: ");
+        input = in.nextLine();
+
+        while (invalid) {
+            if (FormatChecker.canParseInt(input)) {
+                answer = Integer.parseInt(input);
+
+                if (answer < 0) {
+                    System.out.println("Enter a POSITIVE number: ");
+                    input = in.nextLine();
+                } else {
+                    invalid = false;
+                }
+            } else if (!FormatChecker.canParseInt(input)) {
+                System.out.println("Enter a NUMBER: ");
+                input = in.nextLine();
+            }
+        }
+
+        return answer;
+    }
+
+    /**
+     * Calculates square root.
+     *
+     * @param num
+     *            number to do math with
+     * @return valid answer
+     */
+    private static double squareRoot(double num) {
+        return Math.sqrt(num);
+    }
+
+    /**
+     * Prints square root result.
+     *
+     * @param in
+     *            input stream
+     * @param out
+     *            output stream
+     */
+    private static void printRoot(SimpleReader in, SimpleWriter out) {
+        double num = getNumber(in, out);
+        double answer = squareRoot(num);
+
+        System.out.println("The square root of " + num + " is " + answer);
+    }
+
+}
