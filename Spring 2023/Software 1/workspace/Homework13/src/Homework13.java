c3bb7633dbe6d4d7c8b225b3c7f2331572f8f82c
Gage Farmer <naturesninja78@gmail.com>
2023-03-08 14:40:38 -0500

    Update Homework13.java

--- Spring 2023/Software 1/workspace/Homework13/src/Homework13.java
+++ Spring 2023/Software 1/workspace/Homework13/src/Homework13.java
@@ -1,4 +1,5 @@
 import components.naturalnumber.NaturalNumber;
+import components.naturalnumber.NaturalNumber2;
 
 /**
  * Simple HelloWorld program (clear of Checkstyle and SpotBugs warnings).
@@ -25,7 +26,19 @@ public final class Homework13 {
      */
     private static NaturalNumber productOfDigits1(NaturalNumber n) {
 
-        while (n.compareTo()44)
+    	NaturalNumber NINE = new NaturalNumber2(9);
+    	int remainder, productInt = 1;
+    	
+        if (n.compareTo(NINE) == 1) {
+        	remainder = n.divideBy10();
+        	productInt = productInt * remainder;
+        	productOfDigits1(n);
+        }
+        
+        NaturalNumber product = new NaturalNumber2(productInt);
+        n.clear();
+        
+        return product;
     }
 
     /**
@@ -37,7 +50,21 @@ public final class Homework13 {
      * @ensures productOfDigits2 = [product of the digits of n]
      */
     private static NaturalNumber productOfDigits2(NaturalNumber n) {
-
+    	
+    	NaturalNumber NINE = new NaturalNumber2(9);
+    	NaturalNumber n2 = new NaturalNumber2(n);
+    	int remainder, productInt = 1;
+    	
+        if (n.compareTo(NINE) == 1) {
+        	remainder = n2.divideBy10();
+        	productInt = productInt * remainder;
+        	productOfDigits2(n2);
+        }
+        
+        NaturalNumber product = new NaturalNumber2(productInt);
+        
+        
+        return product;
     }
 
     /**
@@ -47,7 +74,34 @@ public final class Homework13 {
      *            the command line arguments; unused here
      */
     public static void main(String[] args) {
-        // TODO: nothing
+    	NaturalNumber x = new NaturalNumber2(44);
+    	NaturalNumber y = new NaturalNumber2(482);
+    	NaturalNumber z = new NaturalNumber2(35);
+    	
+    	System.out.println("Test 1");
+    	System.out.println("Expected: 16 and 44");
+    	System.out.println("Actual:   " + productOfDigits2(x) + " and " + x);
+    	
+    	System.out.println("Test 2");
+    	System.out.println("Expected: 64 and 482");
+    	System.out.println("Actual:   " + productOfDigits2(y) + " and " + y);
+    	
+    	System.out.println("Test 3");
+    	System.out.println("Expected: 15 and 35");
+    	System.out.println("Actual:   " + productOfDigits2(z) + " and " + z);
+    	
+    	
+    	System.out.println("Test 4");
+    	System.out.println("Expected: 16 and 0");
+    	System.out.println("Actual:   " + productOfDigits1(x) + " and " + x);
+    	
+    	System.out.println("Test 5");
+    	System.out.println("Expected: 64 and 0");
+    	System.out.println("Actual:   " + productOfDigits1(y) + " and " + y);
+    	
+    	System.out.println("Test 6");
+    	System.out.println("Expected: 15 and 0");
+    	System.out.println("Actual:   " + productOfDigits1(z) + " and " + z);
     }
 
 }
