3d9ab22f47c4b3bba1aeb9cc9ad8383349e5ed72
Gage Farmer <naturesninja78@gmail.com>
2023-04-05 00:36:48 -0400

    nuts

---
+++ Spring 2023/Software 1/workspace/NextWordOrSeparator/src/Homework19.java
@@ -0,0 +1,92 @@
+import components.set.Set;
+
+/**
+ * awesome
+ *
+ * @author Gage Farmer
+ */
+public final class Homework19 {
+
+    /**
+     * No argument constructor--private to prevent instantiation.
+     */
+    private Homework19() {
+        // no code needed here
+    }
+
+    /**
+     * Generates the set of characters in the given {@code String} into the
+     * given {@code Set}.
+     *
+     * @param str
+     *            the given {@code String}
+     * @param charSet
+     *            the {@code Set} to be replaced
+     * @replaces charSet
+     * @ensures charSet = entries(str)
+     */
+    private static void generateElements(String str, Set<Character> charSet) {
+        int i = 0;
+
+        while (i < str.length()) {
+            charSet.add(str.charAt(i));
+        }
+    }
+
+    /**
+     * Returns the first "word" (maximal length string of characters not in
+     * {@code separators}) or "separator string" (maximal length string of
+     * characters in {@code separators}) in the given {@code text} starting at
+     * the given {@code position}.
+     *
+     * @param text
+     *            the {@code String} from which to get the word or separator
+     *            string
+     * @param position
+     *            the starting index
+     * @param separators
+     *            the {@code Set} of separator characters
+     * @return the first word or separator string found in {@code text} starting
+     *         at index {@code position}
+     * @requires 0 <= position < |text|
+     * @ensures <pre>
+     * nextWordOrSeparator =
+     *   text[position, position + |nextWordOrSeparator|)  and
+     * if entries(text[position, position + 1)) intersection separators = {}
+     * then
+     *   entries(nextWordOrSeparator) intersection separators = {}  and
+     *   (position + |nextWordOrSeparator| = |text|  or
+     *    entries(text[position, position + |nextWordOrSeparator| + 1))
+     *      intersection separators /= {})
+     * else
+     *   entries(nextWordOrSeparator) is subset of separators  and
+     *   (position + |nextWordOrSeparator| = |text|  or
+     *    entries(text[position, position + |nextWordOrSeparator| + 1))
+     *      is not subset of separators)
+     * </pre>
+     */
+    private static String nextWordOrSeparator(String text, int position,
+            Set<Character> separators) {
+
+        boolean done = false;
+        int i = 0;
+        String word = "";
+
+        while (!done) {
+            if (separators.contains(text.charAt(position + i))) {
+                done = true;
+            } else {
+                word = word + text.charAt(position + i);
+            }
+            i++;
+        }
+
+        return word;
+    }
+
+    private static void main() {
+        nextWordOrSeparator(null, 0, null);
+        generateElements(null, null);
+    }
+
+}
