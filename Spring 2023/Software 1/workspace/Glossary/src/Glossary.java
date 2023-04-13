3dbbccc9f5c187eb0049b835a500bdd750fedd1e
Gage Farmer <naturesninja78@gmail.com>
2023-04-13 13:00:41 -0400

    yo mama

--- Spring 2023/Software 1/workspace/Glossary/src/Glossary.java
+++ Spring 2023/Software 1/workspace/Glossary/src/Glossary.java
@@ -1,3 +1,8 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Map.Entry;
+import java.util.TreeMap;
+
 import components.simplereader.SimpleReader;
 import components.simplereader.SimpleReader1L;
 import components.simplewriter.SimpleWriter;
@@ -26,18 +31,14 @@ public final class Glossary {
      * @param out
      *            output file
      */
-    private static void generateTerm(SimpleReader in, SimpleWriter out) {
-        int line = 0;
-        String next = in.nextLine();
+    private static void generateTerm(TreeMap<String, String> in,
+            SimpleWriter out) {
 
-        while (!next.equals("")) {
-            if (line == 0) {
-                // is a term
-                out.println("<a href=\"#term_" + next + "\">" + next + "</a>");
-            }
+        for (Entry<String, String> entry : in.entrySet()) {
+            String term = entry.getKey();
 
-            line++;
-            next = in.nextLine();
+            out.println("<a href=\"#term_" + term + "\">" + term + "</a>");
+
         }
 
     }
@@ -51,28 +52,64 @@ public final class Glossary {
      * @param out
      *            output file
      */
-    private static void generateDefinition(SimpleReader in, SimpleWriter out) {
-        int line = 0;
-        String next = in.nextLine();
-        String term = "";
+    private static void generateDefinition(TreeMap<String, String> in,
+            SimpleWriter out) {
 
-        while (!next.equals("")) {
-            if (line != 0) {
-                out.println("<a id=\"term_" + term + "\">");
-                out.println(term + " - " + next);
-            } else {
-                term = next;
-            }
+        for (Entry<String, String> entry : in.entrySet()) {
+            String term = entry.getKey();
+            String definition = entry.getValue();
 
-            line++;
-            next = in.nextLine();
+            out.println("<a id=\"term_" + term + "\">");
+            out.println(term + " - " + definition);
 
         }
 
     }
 
     /**
-     * skips 10 lines
+     * turn file into a map.
+     *
+     * @param in
+     *            infile
+     * @param length
+     *            length of file
+     * @return map!!!!
+     */
+    private static TreeMap<String, String> generateMap(SimpleReader in,
+            int length) {
+        int line = 0, i = 0;
+        String next = in.nextLine(), term = "", definition = "";
+        TreeMap<String, String> map = new TreeMap<>();
+
+        while (line < length) {
+            while (!next.equals("")) {
+
+                if (i == 0) {
+                    term = in.nextLine();
+                } else {
+                    definition = definition + " " + in.nextLine();
+                }
+
+                line++;
+                i++;
+                next = in.nextLine();
+            }
+
+            map.put(term, definition);
+            term = "";
+            definition = "";
+            i = 0;
+        }
+
+        return null;
+
+    }
+
+    /**
+     * skips 10 lines.
+     *
+     * @param out
+     *            outfile
      */
     private static void nextPage(SimpleWriter out) {
         for (int i = 0; i < 10; i++) {
@@ -81,12 +118,16 @@ public final class Glossary {
     }
 
     /**
-     * Generates the header for the html file
+     * Generates the header for the html file.
+     *
+     * @param out
+     *            outfile!!!!
      */
     public static void generateHeader(SimpleWriter out) {
         out.println("<?xml version=\"1.0\" encoding=\"ISO-8859-1\" ?>");
         out.println(
-                "<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Strict//EN\" \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd\">");
+                "<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Strict//EN\" \"h"
+                        + "ttp://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd\">");
         out.println("<html xmlns=\"http://www.w3.org/1999/xhtml\">");
         out.println();
     }
@@ -96,19 +137,22 @@ public final class Glossary {
      *
      * @param args
      *            the command line arguments
+     * @throws FileNotFoundException
      */
-    public static void main(String[] args) {
-        SimpleReader in1 = new SimpleReader1L("data/terms.txt");
-        SimpleReader in2 = new SimpleReader1L("data/terms.txt");
+    public static void main(String[] args) throws FileNotFoundException {
+        File fileLOL = new File("data/terms.txt");
+        int fileLength = (int) fileLOL.length();
+        SimpleReader in = new SimpleReader1L("data/terms.txt");
         SimpleWriter out = new SimpleWriter1L("data/glossary.html");
+        TreeMap<String, String> map;
 
+        map = generateMap(in, fileLength);
         generateHeader(out);
-        generateTerm(in1, out);
+        generateTerm(map, out);
         nextPage(out);
-        generateDefinition(in2, out);
+        generateDefinition(map, out);
 
-        in1.close();
-        in2.close();
+        in.close();
         out.close();
     }
 
