d05412b74fc60f8d2cc651cd95c56590db1108c7
Gage Farmer <naturesninja78@gmail.com>
2023-04-20 22:16:05 -0400

    yeah idk

---
+++ Spring 2023/Software 1/workspace/NaturalNumberCalculator/src/NNCalcController1.java
@@ -0,0 +1,212 @@
+import components.naturalnumber.NaturalNumber;
+import components.naturalnumber.NaturalNumber2;
+
+/**
+ * Controller class.
+ *
+ * @author Put your name here
+ */
+public final class NNCalcController1 implements NNCalcController {
+
+    /**
+     * Model object.
+     */
+    private final NNCalcModel model;
+
+    /**
+     * View object.
+     */
+    private final NNCalcView view;
+
+    /**
+     * Useful constants.
+     */
+    private static final NaturalNumber TWO = new NaturalNumber2(2),
+            INT_LIMIT = new NaturalNumber2(Integer.MAX_VALUE);
+
+    /**
+     * Updates this.view to display this.model, and to allow only operations
+     * that are legal given this.model.
+     *
+     * @param model
+     *            the model
+     * @param view
+     *            the view
+     * @ensures [view has been updated to be consistent with model]
+     */
+    private static void updateViewToMatchModel(NNCalcModel model,
+            NNCalcView view) {
+
+        /*
+         * Get model info
+         */
+        NaturalNumber input = model.top();
+        NaturalNumber output = model.bottom();
+        /*
+         * Update view to reflect changes in model
+         */
+        view.updateTopDisplay(input);
+        view.updateBottomDisplay(output);
+
+    }
+
+    /**
+     * Constructor.
+     *
+     * @param model
+     *            model to connect to
+     * @param view
+     *            view to connect to
+     */
+    public NNCalcController1(NNCalcModel model, NNCalcView view) {
+        this.model = model;
+        this.view = view;
+        updateViewToMatchModel(model, view);
+    }
+
+    @Override
+    public void processClearEvent() {
+        /*
+         * Get alias to bottom from model
+         */
+        NaturalNumber bottom = this.model.bottom();
+        /*
+         * Update model in response to this event
+         */
+        bottom.clear();
+        /*
+         * Update view to reflect changes in model
+         */
+        updateViewToMatchModel(this.model, this.view);
+    }
+
+    @Override
+    public void processSwapEvent() {
+        /*
+         * Get aliases to top and bottom from model
+         */
+        NaturalNumber top = this.model.top();
+        NaturalNumber bottom = this.model.bottom();
+        /*
+         * Update model in response to this event
+         */
+        NaturalNumber temp = top.newInstance();
+        temp.transferFrom(top);
+        top.transferFrom(bottom);
+        bottom.transferFrom(temp);
+        /*
+         * Update view to reflect changes in model
+         */
+        updateViewToMatchModel(this.model, this.view);
+    }
+
+    @Override
+    public void processEnterEvent() {
+
+        NaturalNumber top = this.model.top();
+        NaturalNumber bottom = this.model.bottom();
+
+        top.transferFrom(bottom);
+        bottom.clear();
+
+        updateViewToMatchModel(this.model, this.view);
+
+    }
+
+    @Override
+    public void processAddEvent() {
+
+        NaturalNumber top = this.model.top();
+        NaturalNumber bottom = this.model.bottom();
+
+        bottom.add(top);
+        top.clear();
+
+        updateViewToMatchModel(this.model, this.view);
+
+    }
+
+    @Override
+    public void processSubtractEvent() {
+
+        NaturalNumber top = this.model.top();
+        NaturalNumber bottom = this.model.bottom();
+
+        top.subtract(bottom);
+        bottom.transferFrom(top);
+        top.clear();
+
+        updateViewToMatchModel(this.model, this.view);
+
+    }
+
+    @Override
+    public void processMultiplyEvent() {
+
+        NaturalNumber top = this.model.top();
+        NaturalNumber bottom = this.model.bottom();
+
+        bottom.multiply(top);
+        top.clear();
+
+        updateViewToMatchModel(this.model, this.view);
+
+    }
+
+    @Override
+    public void processDivideEvent() {
+
+        NaturalNumber top = this.model.top();
+        NaturalNumber bottom = this.model.bottom();
+
+        top.divide(bottom);
+        bottom.transferFrom(top);
+        top.clear();
+
+        updateViewToMatchModel(this.model, this.view);
+
+    }
+
+    @Override
+    public void processPowerEvent() {
+
+        NaturalNumber top = this.model.top();
+        NaturalNumber bottom = this.model.bottom();
+
+        top.power(bottom.toInt());
+        bottom.transferFrom(top);
+        top.clear();
+
+        updateViewToMatchModel(this.model, this.view);
+
+    }
+
+    @Override
+    public void processRootEvent() {
+
+        NaturalNumber top = this.model.top();
+        NaturalNumber bottom = this.model.bottom();
+
+        top.root(bottom.toInt());
+        bottom.transferFrom(top);
+        top.clear();
+
+        updateViewToMatchModel(this.model, this.view);
+
+    }
+
+    @Override
+    public void processAddNewDigitEvent(int digit) {
+
+        NaturalNumber bottom = this.model.bottom();
+
+        String temp = bottom.toString();
+        temp = temp + Integer.toString(digit);
+        NaturalNumber temp2 = new NaturalNumber2(Integer.valueOf(temp));
+        bottom.transferFrom(temp2);
+
+        updateViewToMatchModel(this.model, this.view);
+
+    }
+
+}
---
+++ Spring 2023/Software 1/workspace/NaturalNumberCalculator/src/NNCalcView1.java
@@ -0,0 +1,389 @@
+import java.awt.Cursor;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+
+import components.naturalnumber.NaturalNumber;
+
+/**
+ * View class.
+ *
+ * @author Put your name here
+ */
+public final class NNCalcView1 extends JFrame implements NNCalcView {
+
+    /**
+     * Controller object registered with this view to observe user-interaction
+     * events.
+     */
+    private NNCalcController controller;
+
+    /**
+     * State of user interaction: last event "seen".
+     */
+    private enum State {
+        /**
+         * Last event was clear, enter, another operator, or digit entry, resp.
+         */
+        SAW_CLEAR, SAW_ENTER_OR_SWAP, SAW_OTHER_OP, SAW_DIGIT
+    }
+
+    /**
+     * State variable to keep track of which event happened last; needed to
+     * prepare for digit to be added to bottom operand.
+     */
+    private State currentState;
+
+    /**
+     * Text areas.
+     */
+    private final JTextArea tTop, tBottom;
+
+    /**
+     * Operator and related buttons.
+     */
+    private final JButton bClear, bSwap, bEnter, bAdd, bSubtract, bMultiply,
+            bDivide, bPower, bRoot;
+
+    /**
+     * Digit entry buttons.
+     */
+    private final JButton[] bDigits;
+
+    /**
+     * Useful constants.
+     */
+    private static final int TEXT_AREA_HEIGHT = 5, TEXT_AREA_WIDTH = 20,
+            DIGIT_BUTTONS = 10, MAIN_BUTTON_PANEL_GRID_ROWS = 4,
+            MAIN_BUTTON_PANEL_GRID_COLUMNS = 4, SIDE_BUTTON_PANEL_GRID_ROWS = 3,
+            SIDE_BUTTON_PANEL_GRID_COLUMNS = 1, CALC_GRID_ROWS = 3,
+            CALC_GRID_COLUMNS = 1, BUTT_GRID_ROWS = 1, BUTT_GRID_COLUMNS = 2;
+
+    /**
+     * No argument constructor.
+     */
+    public NNCalcView1() {
+        // Create the JFrame being extended
+
+        /*
+         * Call the JFrame (superclass) constructor with a String parameter to
+         * name the window in its title bar
+         */
+        super("Natural Number Calculator");
+
+        // Set up the GUI widgets --------------------------------------------
+
+        /*
+         * Set up initial state of GUI to behave like last event was "Clear";
+         * currentState is not a GUI widget per se, but is needed to process
+         * digit button events appropriately
+         */
+        this.currentState = State.SAW_CLEAR;
+
+        // TODO: fill in rest of body, following outline in comments
+
+        /*
+         * Create widgets
+         */
+        this.tBottom = new JTextArea("0", TEXT_AREA_HEIGHT, TEXT_AREA_WIDTH);
+        this.tTop = new JTextArea("0", TEXT_AREA_HEIGHT, TEXT_AREA_WIDTH);
+
+        this.bClear = new JButton("Clear");
+        this.bSwap = new JButton("Swap");
+        this.bEnter = new JButton("Enter");
+        this.bAdd = new JButton("+");
+        this.bSubtract = new JButton("-");
+        this.bMultiply = new JButton("x");
+        this.bDivide = new JButton("/");
+        this.bPower = new JButton("Power");
+        this.bRoot = new JButton("Root");
+
+        this.bDigits = new JButton[10];
+        this.bDigits[0] = new JButton("0");
+        this.bDigits[1] = new JButton("1");
+        this.bDigits[2] = new JButton("2");
+        this.bDigits[3] = new JButton("3");
+        this.bDigits[4] = new JButton("4");
+        this.bDigits[5] = new JButton("5");
+        this.bDigits[6] = new JButton("6");
+        this.bDigits[7] = new JButton("7");
+        this.bDigits[8] = new JButton("8");
+        this.bDigits[9] = new JButton("9");
+
+        // Set up the GUI widgets --------------------------------------------
+
+        /*
+         * Text areas should wrap lines, and should be read-only; they cannot be
+         * edited because allowing keyboard entry would require checking whether
+         * entries are digits, which we don't want to have to do
+         */
+
+        this.tTop.setEditable(false);
+        this.tTop.setLineWrap(true);
+        this.tTop.setWrapStyleWord(true);
+        this.tTop.setVisible(true);
+        this.tBottom.setEditable(false);
+        this.tBottom.setLineWrap(true);
+        this.tBottom.setWrapStyleWord(true);
+        this.tBottom.setVisible(true);
+
+        /*
+         * Initially, the following buttons should be disabled: divide (divisor
+         * must not be 0) and root (root must be at least 2) -- hint: see the
+         * JButton method setEnabled
+         */
+
+        this.bDivide.setEnabled(false);
+        this.bRoot.setEnabled(false);
+
+        /*
+         * Create scroll panes for the text areas in case number is long enough
+         * to require scrolling
+         */
+
+        JScrollPane scrollTop = new JScrollPane(this.tTop);
+        JScrollPane scrollBottom = new JScrollPane(this.tBottom);
+
+        /*
+         * Create main button panel
+         */
+
+        JPanel mainPanel = new JPanel(new GridLayout(
+                MAIN_BUTTON_PANEL_GRID_ROWS, MAIN_BUTTON_PANEL_GRID_COLUMNS));
+
+        /*
+         * Add the buttons to the main button panel, from left to right and top
+         * to bottom
+         */
+
+        // brute force yay :)
+        mainPanel.add(this.bDigits[7]);
+        mainPanel.add(this.bDigits[8]);
+        mainPanel.add(this.bDigits[9]);
+        mainPanel.add(this.bAdd);
+        mainPanel.add(this.bDigits[4]);
+        mainPanel.add(this.bDigits[5]);
+        mainPanel.add(this.bDigits[6]);
+        mainPanel.add(this.bSubtract);
+        mainPanel.add(this.bDigits[1]);
+        mainPanel.add(this.bDigits[2]);
+        mainPanel.add(this.bDigits[3]);
+        mainPanel.add(this.bMultiply);
+        mainPanel.add(this.bDigits[0]);
+        mainPanel.add(this.bPower);
+        mainPanel.add(this.bRoot);
+        mainPanel.add(this.bDivide);
+
+        /*
+         * Create side button panel
+         */
+
+        JPanel sidePanel = new JPanel(new GridLayout(
+                SIDE_BUTTON_PANEL_GRID_ROWS, SIDE_BUTTON_PANEL_GRID_COLUMNS));
+
+        /*
+         * Add the buttons to the side button panel, from left to right and top
+         * to bottom
+         */
+
+        sidePanel.add(this.bClear);
+        sidePanel.add(this.bSwap);
+        sidePanel.add(this.bEnter);
+
+        /*
+         * Create combined button panel organized using flow layout, which is
+         * simple and does the right thing: sizes of nested panels are natural,
+         * not necessarily equal as with grid layout
+         */
+
+        JPanel combinedPanel = new JPanel(
+                new GridLayout(BUTT_GRID_ROWS, BUTT_GRID_COLUMNS));
+
+        /*
+         * Add the other two button panels to the combined button panel
+         */
+
+        combinedPanel.add(mainPanel);
+        combinedPanel.add(sidePanel);
+
+        /*
+         * Organize main window
+         */
+
+        this.setLayout(new GridLayout(CALC_GRID_ROWS, CALC_GRID_COLUMNS));
+
+        /*
+         * Add scroll panes and button panel to main window, from left to right
+         * and top to bottom
+         */
+
+        this.add(scrollTop);
+        this.add(scrollBottom);
+        this.add(combinedPanel);
+
+        // Set up the observers ----------------------------------------------
+
+        /*
+         * Register this object as the observer for all GUI events
+         */
+
+        this.bClear.addActionListener(this);
+        this.bSwap.addActionListener(this);
+        this.bEnter.addActionListener(this);
+        this.bAdd.addActionListener(this);
+        this.bSubtract.addActionListener(this);
+        this.bMultiply.addActionListener(this);
+        this.bDivide.addActionListener(this);
+        this.bPower.addActionListener(this);
+        this.bRoot.addActionListener(this);
+        this.bDigits[0].addActionListener(this);
+        this.bDigits[1].addActionListener(this);
+        this.bDigits[2].addActionListener(this);
+        this.bDigits[3].addActionListener(this);
+        this.bDigits[4].addActionListener(this);
+        this.bDigits[5].addActionListener(this);
+        this.bDigits[6].addActionListener(this);
+        this.bDigits[7].addActionListener(this);
+        this.bDigits[8].addActionListener(this);
+        this.bDigits[9].addActionListener(this);
+
+        // Set up the main application window --------------------------------
+
+        /*
+         * Make sure the main window is appropriately sized, exits this program
+         * on close, and becomes visible to the user
+         */
+
+        this.setSize(HEIGHT, WIDTH);
+        this.pack();
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setVisible(true);
+
+    }
+
+    @Override
+    public void registerObserver(NNCalcController controller) {
+
+        this.controller = controller;
+
+    }
+
+    @Override
+    public void updateTopDisplay(NaturalNumber n) {
+
+        this.tTop.setText(n.toString());
+
+    }
+
+    @Override
+    public void updateBottomDisplay(NaturalNumber n) {
+
+        this.tBottom.setText(n.toString());
+
+    }
+
+    @Override
+    public void updateSubtractAllowed(boolean allowed) {
+
+        this.bSubtract.setEnabled(allowed);
+
+    }
+
+    @Override
+    public void updateDivideAllowed(boolean allowed) {
+
+        this.bDivide.setEnabled(allowed);
+
+    }
+
+    @Override
+    public void updatePowerAllowed(boolean allowed) {
+
+        this.bPower.setEnabled(allowed);
+
+    }
+
+    @Override
+    public void updateRootAllowed(boolean allowed) {
+
+        this.bRoot.setEnabled(allowed);
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent event) {
+        /*
+         * Set cursor to indicate computation on-going; this matters only if
+         * processing the event might take a noticeable amount of time as seen
+         * by the user
+         */
+        this.setCursor(Cursor.getPredefinedCursor(Cursor.WAIT_CURSOR));
+        /*
+         * Determine which event has occurred that we are being notified of by
+         * this callback; in this case, the source of the event (i.e, the widget
+         * calling actionPerformed) is all we need because only buttons are
+         * involved here, so the event must be a button press; in each case,
+         * tell the controller to do whatever is needed to update the model and
+         * to refresh the view
+         */
+        Object source = event.getSource();
+        if (source == this.bClear) {
+            this.controller.processClearEvent();
+            this.currentState = State.SAW_CLEAR;
+        } else if (source == this.bSwap) {
+            this.controller.processSwapEvent();
+            this.currentState = State.SAW_ENTER_OR_SWAP;
+        } else if (source == this.bEnter) {
+            this.controller.processEnterEvent();
+            this.currentState = State.SAW_ENTER_OR_SWAP;
+        } else if (source == this.bAdd) {
+            this.controller.processAddEvent();
+            this.currentState = State.SAW_OTHER_OP;
+        } else if (source == this.bSubtract) {
+            this.controller.processSubtractEvent();
+            this.currentState = State.SAW_OTHER_OP;
+        } else if (source == this.bMultiply) {
+            this.controller.processMultiplyEvent();
+            this.currentState = State.SAW_OTHER_OP;
+        } else if (source == this.bDivide) {
+            this.controller.processDivideEvent();
+            this.currentState = State.SAW_OTHER_OP;
+        } else if (source == this.bPower) {
+            this.controller.processPowerEvent();
+            this.currentState = State.SAW_OTHER_OP;
+        } else if (source == this.bRoot) {
+            this.controller.processRootEvent();
+            this.currentState = State.SAW_OTHER_OP;
+        } else {
+            for (int i = 0; i < DIGIT_BUTTONS; i++) {
+                if (source == this.bDigits[i]) {
+                    switch (this.currentState) {
+                        case SAW_ENTER_OR_SWAP:
+                            this.controller.processClearEvent();
+                            break;
+                        case SAW_OTHER_OP:
+                            this.controller.processEnterEvent();
+                            this.controller.processClearEvent();
+                            break;
+                        default:
+                            break;
+                    }
+                    this.controller.processAddNewDigitEvent(i);
+                    this.currentState = State.SAW_DIGIT;
+                    break;
+                }
+            }
+        }
+        /*
+         * Set the cursor back to normal (because we changed it at the beginning
+         * of the method body)
+         */
+        this.setCursor(Cursor.getDefaultCursor());
+    }
+
+}
