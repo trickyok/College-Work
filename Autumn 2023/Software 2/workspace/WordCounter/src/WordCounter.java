449117cd8e3561b4499c97ec4d9b147b5f493bb0
Gage Farmer <naturesninja78@gmail.com>
2023-08-31 23:31:58 -0400

    yes

--- Autumn 2023/Software 2/workspace/WordCounter/src/WordCounter.java
+++ Autumn 2023/Software 2/workspace/WordCounter/src/WordCounter.java
@@ -1,4 +1,10 @@
-import components.map.Map;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.TreeMap;
+
+import components.queue.Queue;
+import components.queue.Queue1L;
 import components.simplereader.SimpleReader;
 import components.simplereader.SimpleReader1L;
 import components.simplewriter.SimpleWriter;
@@ -14,89 +20,162 @@ import components.simplewriter.SimpleWriter1L;
 public final class WordCounter {
 
     /**
+     * whitespace.
+     *
+     */
+    private static String[] WHITESPACE = { "-", " ", ".", ",", "!", "?", ";",
+            "$", "%" };
+
+    /**
      * No argument constructor--private to prevent instantiation.
      */
     private WordCounter() {
     }
 
     /**
-     * Adds word to the map, increments value if already in.
+     * Scans through the input to get all words.
      *
-     * @param map
+     * @param words
+     * @param input
      *
-     * @param word
-     *
-     * @return number of occurrences of 'word' in 'map'
      */
-    private static int addToMap(Map map, String word) {
+    private static void getWords(SimpleReader input, Queue<String> words) {
 
-        // TODO
+        /*
+         * Scan through the file and return only space-less lines
+         */
+        while (!input.atEOS()) {
+            String temp = input.nextLine();
 
-        return -1;
+            if (!(temp.contains(" ") && temp.isEmpty())) {
+                String[] subList = temp.split(" ");
+
+                for (int idx = 0; idx < subList.length; idx++) {
+                    String[] cleanedWords = cleanWord(subList[idx]);
+                    for (String clean : cleanedWords) {
+                        words.enqueue(clean);
+                    }
+
+                }
+            }
+
+        }
     }
 
     /**
-     * Searches map for word.
-     *
-     * @param map
+     * helper method for getWords, just cleans up the list by removing any
+     * special characters, and splitting any combined words.
      *
      * @param word
-     *
-     * @return if 'word' is in 'map'
+     *            words
+     * @return cleaned word
      */
-    private static boolean inMap(Map map, String word) {
+    private static String[] cleanWord(String word) {
 
-        // TODO
+        // gee i sure hope there isn't an edge case where the array of 10 is too small!
+        Boolean t = true;
+        String[] result = null;
+        word = word.toLowerCase();
 
-        return false;
+        for (String special : WHITESPACE) {
+            if (word.contains(special) && t == true) {
+                result = word.split(special);
+            }
+        }
+        if (result == null) {
+            result = word.split("SUPERDUPERTOPSECRETPHRASE!!!!DON'TTYPEMEEE");
+        }
 
+        return result;
     }
 
     /**
-     * Sorts map alphabetically.
+     * Converts two queues to one sorted treemap
      *
-     * @param map
+     * @param word
+     * @param num
      */
-    private static void sortMap(Map map) {
+    @SuppressWarnings("unchecked")
+    private static TreeMap queueToTreeMap(Queue<String> word) {
 
-        // TODO
+        @SuppressWarnings("rawtypes")
+        TreeMap list = new TreeMap();
+
+        /*
+         * Loops through queues and adds each value to a list
+         */
+        while (word.length() > 0) {
+            String temp = word.dequeue();
+
+            if (list.containsKey(temp) && temp != " ") {
+                int val = (int) list.remove(temp);
+                val++;
+                list.put(temp, val);
+            } else {
+                list.put(temp, 1);
+            }
+        }
+
+        return list;
 
     }
 
     /**
      * Formats and prints the graph into html.
      *
-     * @param map
-     *
-     * @param file
+     * @param list
+     * @param output
+     * @throws IOException
      */
-    private static void printGraph(Map map, String file) {
+    private static void printGraph(TreeMap list, FileWriter output)
+            throws IOException {
 
-        // TODO
+        /*
+         * Intentionally skipping the first key and value (it is whitespace)
+         */
+        String key = (String) list.firstKey();
+        int value = (int) list.get(key);
+        list.remove(key);
+        int idx = 0;
+
+        output.write(
+                "<table style=margin-left:auto;margin-right:auto;> \n <tr> <th>"
+                        + "Word</th> <th>Occurances</th> </tr> \n");
+
+        while (list.size() > 0) {
+            key = (String) list.firstKey();
+            value = (int) list.get(key);
+            output.write(
+                    "<tr> <th>" + key + "</th><th>" + value + "</th> </tr>\n");
+            list.remove(key);
+        }
 
     }
 
     /**
      * creates and outputs html header.
      *
-     * @param file
+     * @param output
      *            file to print to
+     * @param fileName
+     *            name of de file
+     * @throws IOException
      */
-    private static void printHeader(String file) {
+    private static void printHeader(FileWriter output, String fileName)
+            throws IOException {
 
-        // TODO
+        output.write("<!DOCTYPE html>\n");
+        output.write("<html>\n" + "<style>\n" + "table, th, td {\n"
+                + "     border:2px solid red;color:#FFFFFF;\n"
 
-    }
-
-    /**
-     * creates and outputs html footer.
-     *
-     * @param file
-     *            file to print to
-     */
-    private static void printFooter(String file) {
-
-        // TODO
+                + "}\n" + "#grad1 {\n" + "  height: 55px;\n"
+                + "  background-color: red;\n"
+                + "  background-image: linear-gradient(to right, red, orange, "
+                + "yellow, violet);\n" + "}\n" + "</style>\n" + "<head>\n"
+                + "<div id=\"grad1\" style=\"text-align:center;margin:auto;"
+                + "color:#FFFFFF;font-size:40px;font-weight:bold\">\n"
+                + "Words Counted in " + fileName + "\n" + "</div>" + "</head>\n"
+                + "<body style=\"background-color:#353535;\">\n");
 
     }
 
@@ -105,16 +184,56 @@ public final class WordCounter {
      *
      * @param args
      *            the command line arguments
+     * @throws IOException
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         SimpleReader in = new SimpleReader1L();
         SimpleWriter out = new SimpleWriter1L();
+
+        /*
+         * Gonna try using queues to keep track of these things
+         */
+        Queue<String> words = new Queue1L<>();
+
+        /*
+         * Gets name of file and opens it
+         */
+        out.println("Enter file name: ");
+        String name = in.nextLine();
+        SimpleReader input = new SimpleReader1L(name);
+
+        /*
+         * Gets name of output folder
+         */
+        out.println("Enter the location of the output folder: ");
+        String name2 = in.nextLine();
+
+        /*
+         * Creates html file
+         */
+        File file = new File(name2 + "/wordcount.html");
+        FileWriter output = new FileWriter(name2 + "/wordcount.html");
+        printHeader(output, name);
+
+        /*
+         * Gets word data + occurrences to a list
+         */
+        getWords(input, words);
+        TreeMap list = queueToTreeMap(words);
+
+        /*
+         * Turn that list into an HTML graph Print html footer
+         */
+        printGraph(list, output);
 
         /*
          * Close input and output streams
          */
         in.close();
+        input.close();
         out.close();
+        output.close();
+
     }
 
 }
