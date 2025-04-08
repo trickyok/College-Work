e9743849236c0765f2850ad5770e689456f47235
Gage Farmer <charlesgagefarmer@gmail.com>
2025-04-08 17:33:25 -0400

    yea

--- Spring-2025/Systems 2/Lab/Lab 4/bounded_buffer.c
+++ Spring-2025/Systems 2/Lab/Lab 4/bounded_buffer.c
@@ -4,50 +4,53 @@
 
 /*Do not use any global variables for implementation*/
 
-void bounded_buffer_init(struct bounded_buffer *buffer, int size){
-	buffer->head = 0;
-	buffer->tail = 0;
-	buffer->count = 0;
+void bounded_buffer_init(struct bounded_buffer *b, int size){
+	b->buffer = malloc(sizeof(void *) * size);
+	b->size = size;
+	b->head = 0;
+	b->tail = 0;
+	b->count = 0;
 	
-	pthread_mutex_init(&buffer->lock, NULL);
-	pthread_cond_init(&buffer->not_full);
-	pthread_cond_init(&buffer->not_empty);
+	pthread_mutex_init(&b->lock, NULL);
+	pthread_cond_init(&b->not_full, NULL);
+	pthread_cond_init(&b->not_empty, NULL);
 }
 
-void bounded_buffer_push(struct bounded_buffer *buffer, void *item){
-	pthread_mutex_lock(&buffer->lock);
+void bounded_buffer_push(struct bounded_buffer *b, void *item){
+	pthread_mutex_lock(&b->lock);
 	
-	while (buffer->count == BUFFER_SIZE) {
-		pthread_cond_wait(&buffer->not_full, &buffer->lock);
+	while (b->count == b->size) {
+		pthread_cond_wait(&b->not_full, &b->lock);
 	}
 	
-	buffer->*buffer[buffer->tail] = item;
-	buffer->tail = (buffer->tail + 1) % BUFFER_SIZE;
-	buffer->count++;
+	b->buffer[b->tail] = item;
+	b->tail = (b->tail + 1) % b->size;
+	b->count++;
 	
-	pthread_cond_signal(&buffer->not_empty);
-	pthread_mutex_unlock(&buffer->lock);
+	pthread_cond_signal(&b->not_empty);
+	pthread_mutex_unlock(&b->lock);
 }
 
-void* bounded_buffer_pop(struct bounded_buffer *buffer){
-    pthread_mutex_lock(&buffer->lock);
+void* bounded_buffer_pop(struct bounded_buffer *b){
+    pthread_mutex_lock(&b->lock);
     
-    while (buffer->count=0) {
-    	pthread_cond_wait(&buffer->not_empty, &buffer->lock);
+    while (b->count == 0) {
+    	pthread_cond_wait(&b->not_empty, &b->lock);
     }
     
-    void *item = buffer->buffer[buffer->head];
-    buffer->head = (buffer->head + 1) % BUFFER_SIZE;
-    buffer->count--;
+    void *item = b->buffer[b->head];
+    b->head = (b->head + 1) % b->size;
+    b->count--;
     
-    pthread_cond_signal(&buffer->not_full);
-    pthread_mutex_unlock(&buffer->lock);
+    pthread_cond_signal(&b->not_full);
+    pthread_mutex_unlock(&b->lock);
     
     return item;
 }
 
-void bounded_buffer_destroy(struct bounded_buffer *buffer){
-	pthread_mutex_destroy(&buffer->lock);
-	pthread_cond_destroy(&buffer->not_full);
-	pthread_cond_destroy(&buffer->not_empty);
+void bounded_buffer_destroy(struct bounded_buffer *b){
+	free(b->buffer);
+	pthread_mutex_destroy(&b->lock);
+	pthread_cond_destroy(&b->not_full);
+	pthread_cond_destroy(&b->not_empty);
 }
--- Spring-2025/Systems 2/Lab/Lab 4/main.c
+++ Spring-2025/Systems 2/Lab/Lab 4/main.c
@@ -4,15 +4,36 @@
 #include <pthread.h>
 #include <unistd.h>
 
+#define NUM_PRODUCERS 3
+#define NUM_CONSUMERS 2
+#define MESSAGES_PER_PRODUCER 10
+
 struct bounded_buffer queue;
 
 void *producer(void *ptr);
 void *consumer(void *ptr);
 
 int main() {
-    /* initialize the queue */
+    srand(time(NULL));
+    bounded_buffer_init(&queue, NUM_PRODUCERS + NUM_CONSUMERS);
+    
+    pthread_t producers[NUM_PRODUCERS];
+    pthread_t consumers[NUM_CONSUMERS];
 
-    /* create producer and consumer threads */
+    for (int i = 0; i < NUM_PRODUCERS; i++) {
+        int *id = malloc(sizeof(int));
+        *id = i;
+        pthread_create(&producers[i], NULL, producer, id);
+    }
+
+    for (int i = 0; i < NUM_CONSUMERS; i++) {
+        int *id = malloc(sizeof(int));
+        *id = i;
+        pthread_create(&consumers[i], NULL, consumer, id);
+    }
+
+    sleep(5);
+    bounded_buffer_destroy(&queue);
 	
     return 0;
 }
@@ -20,11 +41,32 @@ int main() {
 /* this is the function executed by the producer thread. 
    It should generate a number of messages and push them into the queue */
 void *producer(void *ptr){
+    int id = *(int *)ptr;
+    free(ptr);
+
+    for (int i = 0; i < MESSAGES_PER_PRODUCER; i++) {
+        int *msg = malloc(sizeof(int));
+        *msg = id * 100 + i;
+        bounded_buffer_push(&queue, msg);
+        printf("Producer %d pushed %d\n", id, *msg);
+        usleep(rand() % 500000);
+    }
+
     return NULL;
 }
 
 /* this is the function executed by the consumer thread. 
    It should pop messages from the queue and print them */
 void *consumer(void *ptr){
+    int id = *(int *)ptr;
+    free(ptr);
+
+    while (1) {
+        int *msg = (int *)bounded_buffer_pop(&queue);
+        printf("Consumer %d popped %d\n", id, *msg);
+        free(msg);
+        usleep(rand() % 500000);
+    }
+
     return NULL;
 }
